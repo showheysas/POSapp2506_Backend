d3ab738d04bbab0fbedf34c5403c33fa6486bab1
showheysas <shoheysas@gmail.com>
2025-06-10 04:25:41 +0900

    startup.sh修正

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/site/wwwroot
-
-# Python 実行ファイルのフルパスで pip を使う（App Service対応）
 python3.11 -m pip install -r requirements.txt
-
-# FastAPI アプリを起動
 python3.11 -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
