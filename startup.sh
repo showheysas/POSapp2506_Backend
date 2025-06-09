d3aed66c06b3a8be29d77c93b01cc2ff47b75586
showheysas <shoheysas@gmail.com>
2025-06-10 04:15:16 +0900

    startup.shをpython3.11 -m pip install -r requirements.txtに修正

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /home/site/wwwroot
-pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
+
+# Python 実行ファイルのフルパスで pip を使う（App Service対応）
+python3.11 -m pip install -r requirements.txt
+
+# FastAPI アプリを起動
+python3.11 -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
