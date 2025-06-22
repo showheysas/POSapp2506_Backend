60443cd90fdd5c11544fb8bb2d8a1b69c9cfd29a
showheysas <shoheysas@gmail.com>
2025-06-23 00:09:47 +0900

    CORS設定調整

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://posapp2506-frontend-r2.vercel.app"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://posapp2506-frontend-r2.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
